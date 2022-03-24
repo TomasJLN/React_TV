01b96cd2805c831bf15ad9d2787e26cd189439b5
Tomás Lago <tomas.lago@mundo-r.com>
2022-03-24 12:35:44 +0100

    to build

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import { useState } from 'react';
-import GetPersons from './GetPersons';
-import SearchByName from './SearchByName';
-import './App.css';
+import { useState } from "react";
+import GetPersons from "./GetPersons";
+import SearchByName from "./SearchByName";
+import "./App.css";
 
 function App() {
   const [searchName, setSearchName] = useState();
 
   return (
     <>
-      <h1>App TV </h1>
+      <h1>App Celebrity TV from api.tvmaze.com</h1>
       <hr />
       <SearchByName setSearchName={setSearchName} />
       <GetPersons searchName={searchName} />
